74ce3eb07415d6c81cf2322491b9a7fab37b4c70
eri-black <109592236+eri-black@users.noreply.github.com>
2022-07-21 14:11:44 +0700

    два сервиса + по папкам

---
+++ stand/scripts/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#editing welcome info
+cp -f /opt/stand/scripts/motd /etc/motd
+
+#creating user and saving his auth info to files
+pass=$(makepasswd)
+login=student$(( $RANDOM % 999 + 1 ))
+echo "$(hostname) $login $pass" > /opt/stand/acc/$(hostname).txt
+rm /opt/stand/acc/acc.txt
+cd /opt/stand/acc/
+cat $(ls /opt/stand/acc/) > /opt/stand/acc/acc.txt
+useradd -p $pass -m -s /bin/bash $login
+echo $login:$pass | chpasswd
+
+#ssh restarting
+chown root:root /run/sshd
+chmod 711 /run/sshd
+service ssh restart
+
+#getting access to the device
+chmod ugo+rwx /dev/ttyUSB*
+
+#anti-closing
+tail -f /dev/null
